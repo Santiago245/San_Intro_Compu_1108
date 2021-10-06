01ef36e83ad6c134118f20832962bab1e1a2b627
Santiago245 <91904076+Santiago245@users.noreply.github.com>
2021-10-06 10:59:17 -0500

    Add files via upload

---
+++ HolaMundo2021.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    float base;
+    float altura;
+    float area;
+    printf("Areas \n ==================\n");
+    //Lectura de datos
+    printf("Base: ");
+    scanf_s("%f",&base);
+
+    printf("Altura");
+    scanf_s("%f", &altura);
+    //Calculo Area
+    area = base * altura;
+    printf("El area de %10f por %10f es :%10f ",base,altura,area);
+
+}
