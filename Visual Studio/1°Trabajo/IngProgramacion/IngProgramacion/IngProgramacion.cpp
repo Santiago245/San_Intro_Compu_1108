b5c0efd7e7dbd335ac38381a3a5e7de402fef2c2
Santiago245 <santiagohernandez28@aragon.unam.mx>
2021-10-11 11:16:50 -0500

    Es un programa que suma dos datos

---
+++ Visual Studio/1°Trabajo/IngProgramacion/IngProgramacion/IngProgramacion.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int Dato1;
+	int Dato2;
+	int Resultado;
+	printf("\n-----------SUMA DE DATOS--------\n");
+	printf_s("Ingresa un dato entero: ");
+	scanf_s("%i", &Dato1);
+	printf("ingresa un segundo dato: ");
+	scanf_s("%i", &Dato2);
+	Resultado = Dato1 + Dato2;
+	printf("El resultado de la suma de %i mas %i es %i:", Dato1, Dato2, Resultado);
+}
